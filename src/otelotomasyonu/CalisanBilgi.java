4f8b2f6597f55c6e154df3642b488f38e0f53c8b
Sedefnaz Durukan <sdfnzdrkn@gmail.com>
2025-12-28 00:29:27 +0300

    Calisan bilgileri classı güncellendi

--- src/otelotomasyonu/CalisanBilgi.java
+++ src/otelotomasyonu/CalisanBilgi.java
@@ -14,7 +14,7 @@ public class CalisanBilgi extends JFrame {
         anaPanel.setBounds(5, 5, 990, 590);
         anaPanel.setBackground(new Color(3, 45, 48));
         anaPanel.setLayout(null);
-        add(anaPanel);
+        getContentPane().add(anaPanel);
 
         // Verilerin Listeleneceği Tablo
         JTable veriTablosu = new JTable();
@@ -23,12 +23,12 @@ public class CalisanBilgi extends JFrame {
         veriTablosu.setBackground(new Color(3, 45, 48));
         anaPanel.add(veriTablosu);
 
-        // Veritabanından Veri Çekme İşlemi
+        // Veritabanından Veri Çekme 
         try {
-            // Sizin verdiğiniz 'baglanti' sınıfından nesne oluşturuluyor
+            // baglanti sınıfından nesne oluşturuluyor
             baglanti b = new baglanti(); 
             
-            // Veritabanı tablonuzun adı "employee" ise bu sorgu çalışır
+            // Veritabanı tablonuzun adı "employee" ise bu sorgu çalışır calısana cevirmek gerek
             String sorgu = "select * from employee"; 
             
             // baglanti sınıfındaki 's' (Statement) kullanılıyor
@@ -57,57 +57,53 @@ public class CalisanBilgi extends JFrame {
         });
 
         // Tablo Başlık Etiketleri (Görsel Düzenleme İçin)
-        JLabel etiketAd = new JLabel("İsim");
-        etiketAd.setBounds(41, 11, 70, 19);
+        JLabel etiketAd = new JLabel("Ad Soyad");
+        etiketAd.setBounds(136, 11, 70, 19);
         etiketAd.setForeground(Color.WHITE);
         etiketAd.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketAd);
 
         JLabel etiketYas = new JLabel("Yaş");
-        etiketYas.setBounds(159, 11, 70, 19);
+        etiketYas.setBounds(273, 11, 70, 19);
         etiketYas.setForeground(Color.WHITE);
         etiketYas.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketYas);
 
-        JLabel etiketCinsiyet = new JLabel("Cinsiyet");
-        etiketCinsiyet.setBounds(273, 11, 70, 19);
-        etiketCinsiyet.setForeground(Color.WHITE);
-        etiketCinsiyet.setFont(new Font("Tahoma", Font.BOLD, 14));
-        anaPanel.add(etiketCinsiyet);
+     
 
         JLabel etiketGorev = new JLabel("Görev");
-        etiketGorev.setBounds(416, 11, 70, 19);
+        etiketGorev.setBounds(434, 11, 70, 19);
         etiketGorev.setForeground(Color.WHITE);
         etiketGorev.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketGorev);
 
         JLabel etiketMaas = new JLabel("Maaş");
-        etiketMaas.setBounds(536, 11, 70, 19);
+        etiketMaas.setBounds(910, 11, 70, 19);
         etiketMaas.setForeground(Color.WHITE);
         etiketMaas.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketMaas);
 
         JLabel etiketTelefon = new JLabel("Telefon");
-        etiketTelefon.setBounds(656, 11, 70, 19);
+        etiketTelefon.setBounds(598, 11, 70, 19);
         etiketTelefon.setForeground(Color.WHITE);
         etiketTelefon.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketTelefon);
 
         JLabel etiketEposta = new JLabel("E-posta");
-        etiketEposta.setBounds(786, 11, 70, 19);
+        etiketEposta.setBounds(750, 11, 70, 19);
         etiketEposta.setForeground(Color.WHITE);
         etiketEposta.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketEposta);
 
         JLabel etiketTC = new JLabel("TC No");
-        etiketTC.setBounds(896, 11, 70, 19);
+        etiketTC.setBounds(29, 11, 70, 19);
         etiketTC.setForeground(Color.WHITE);
         etiketTC.setFont(new Font("Tahoma", Font.BOLD, 14));
         anaPanel.add(etiketTC);
 
         // Pencere Genel Ayarları
         setUndecorated(true);
-        setLayout(null);
+        getContentPane().setLayout(null);
         setLocation(430, 100);
         setSize(1000, 600);
         setVisible(true);
