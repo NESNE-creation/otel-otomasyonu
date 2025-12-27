79ef561c4b19fa13b4d593f8e032a456c5df7957
Sedefnaz Durukan <sdfnzdrkn@gmail.com>
2025-12-28 00:28:28 +0300

    Yonetici bilgileri classı güncellendi

--- src/otelotomasyonu/YoneticiBilgi.java
+++ src/otelotomasyonu/YoneticiBilgi.java
@@ -13,7 +13,7 @@ public class YoneticiBilgi extends JFrame {
         panel.setBounds(5,5,990,590);
         panel.setBackground(new Color(3,45,48));
         panel.setLayout(null);
-        add(panel);
+        getContentPane().add(panel);
 
         // Yönetici Verilerinin Listeleneceği Tablo
         JTable tablo = new JTable();
@@ -55,56 +55,44 @@ public class YoneticiBilgi extends JFrame {
 
         // Tablo Sütun Başlıkları (Türkçe)
         JLabel adLabel = new JLabel("İsim");
-        adLabel.setBounds(41,11,70,19);
+        adLabel.setBounds(212,5,70,19);
         adLabel.setForeground(Color.WHITE);
         adLabel.setFont(new Font("Tahoma", Font.BOLD,14));
         panel.add(adLabel);
 
-        JLabel yasLabel = new JLabel("Yaş");
-        yasLabel.setBounds(159,11,70,19);
-        yasLabel.setForeground(Color.WHITE);
-        yasLabel.setFont(new Font("Tahoma", Font.BOLD,14));
-        panel.add(yasLabel);
-
-        JLabel cinsiyetLabel = new JLabel("Cinsiyet");
-        cinsiyetLabel.setBounds(273,11,70,19);
-        cinsiyetLabel.setForeground(Color.WHITE);
-        cinsiyetLabel.setFont(new Font("Tahoma", Font.BOLD,14));
-        panel.add(cinsiyetLabel);
-
         JLabel gorevLabel = new JLabel("Görev");
-        gorevLabel.setBounds(416,11,70,19);
+        gorevLabel.setBounds(400,5,70,19);
         gorevLabel.setForeground(Color.WHITE);
         gorevLabel.setFont(new Font("Tahoma", Font.BOLD,14));
         panel.add(gorevLabel);
 
         JLabel maasLabel = new JLabel("Maaş");
-        maasLabel.setBounds(536,11,70,19);
+        maasLabel.setBounds(895,5,70,19);
         maasLabel.setForeground(Color.WHITE);
         maasLabel.setFont(new Font("Tahoma", Font.BOLD,14));
         panel.add(maasLabel);
 
         JLabel telefonLabel = new JLabel("Telefon");
-        telefonLabel.setBounds(656,11,70,19);
+        telefonLabel.setBounds(585,5,70,19);
         telefonLabel.setForeground(Color.WHITE);
         telefonLabel.setFont(new Font("Tahoma", Font.BOLD,14));
         panel.add(telefonLabel);
 
         JLabel gmailLabel = new JLabel("Gmail");
-        gmailLabel.setBounds(786,11,70,19);
+        gmailLabel.setBounds(772,5,70,19);
         gmailLabel.setForeground(Color.WHITE);
         gmailLabel.setFont(new Font("Tahoma", Font.BOLD,14));
         panel.add(gmailLabel);
 
-        JLabel tcLabel = new JLabel("TC No");
-        tcLabel.setBounds(896,11,70,19);
+        JLabel tcLabel = new JLabel("TC");
+        tcLabel.setBounds(65,5,70,19);
         tcLabel.setForeground(Color.WHITE);
         tcLabel.setFont(new Font("Tahoma", Font.BOLD,14));
         panel.add(tcLabel);
 
         // Pencere Genel Ayarları
         setUndecorated(true);
-        setLayout(null);
+        getContentPane().setLayout(null);
         setLocation(430,100);
         setSize(1000,600);
         setVisible(true);
